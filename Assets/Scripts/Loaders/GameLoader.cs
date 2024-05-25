e2fadb4fe9398a25847cded889a2216701cec89d
inbarbahnof <158153825+inbarbahnof@users.noreply.github.com>
2024-05-25 12:56:47 +0300

    load player from script

--- Assets/Scripts/Loaders/GameLoader.cs
+++ Assets/Scripts/Loaders/GameLoader.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Charachters;
 using Managers;
 using Loaders;
 using UnityEngine;
@@ -10,11 +11,14 @@ namespace Loaders{
     public class GameLoader : MonoBehaviour
     {
         [SerializeField] private LoaderUI loaderUI;
+        [SerializeField] private PlayerMovement playerPrefab;
+        
+        private PlayerMovement player;
         
         private void Start()
         {
-            // DontDestroyOnLoad(gameObject);
-            // DontDestroyOnLoad(loaderUI.transform.root.gameObject);
+            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(loaderUI.transform.root.gameObject);
             
             loaderUI.Init(100);
             LoadLogicManager();
@@ -40,8 +44,36 @@ namespace Loaders{
         
         private void LoadMainScene()
         {
-            loaderUI.AddProgress(50);
+            loaderUI.AddProgress(10);
+            SceneManager.sceneLoaded += OnMainSceneLoaded;
             SceneManager.LoadScene("MainGame");
+        }
+
+        private void OnMainSceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+            SceneManager.sceneLoaded -= OnMainSceneLoaded;
+            loaderUI.AddProgress(20);
+
+            LoadPlayer();
+            
+            OnLoadComplete();
+        }
+
+        private void LoadPlayer()
+        {
+            if (playerPrefab == null)
+            {
+                Debug.LogError("Player prefab is not assigned in the inspector.");
+                return;
+            }
+            player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+            loaderUI.AddProgress(20);
+        }
+
+        private void OnLoadComplete()
+        {
+            Destroy(this.gameObject);
+            Destroy(loaderUI.transform.root.gameObject);
         }
     }
 }
