e385dbfc7199cc110fc8ecf19b24e68a298080a9
inbarbahnof <158153825+inbarbahnof@users.noreply.github.com>
2024-06-29 15:41:59 +0300

    sun shader

--- Assets/Scripts/Characters/SunController.cs
+++ Assets/Scripts/Characters/SunController.cs
@@ -9,16 +9,43 @@ namespace Characters
     public class SunController : MonoBehaviour
     {
         private const float LIVE_DURATION = 4f;
+        
+        [SerializeField] private float _dissolveTime = 1f;
+
+        private SpriteRenderer _sprite;
+        private Material _material;
+        
+
+        private int _disolveAmount = Shader.PropertyToID("_DissolveAmount");
 
         private void Start()
         {
+            _sprite = GetComponent<SpriteRenderer>();
+            _material = _sprite.material;
+            _material.SetFloat(_disolveAmount, 0);
+            
             StartCoroutine(Die());
+        }
+
+        private IEnumerator Vanish()
+        {
+            float elapsedTime = 0f;
+            while (elapsedTime < _dissolveTime)
+            {
+                elapsedTime += Time.deltaTime;
+                float lerpedDissolve = Mathf.Lerp(0f, 1.1f, (elapsedTime / _dissolveTime));
+                
+                _material.SetFloat(_disolveAmount, lerpedDissolve);
+
+                yield return null;
+            }
+            Destroy(gameObject);
         }
 
         private IEnumerator Die()
         {
             yield return new WaitForSeconds(LIVE_DURATION);
-            Destroy(gameObject);
+            StartCoroutine(Vanish());
         }
 
         private void OnTriggerEnter2D(Collider2D other)
@@ -28,7 +55,7 @@ namespace Characters
                 PlayerController player = other.GetComponent<PlayerController>();
                 player.AddEnergy(0.3f);
                 StopCoroutine(Die());
-                Destroy(gameObject);
+                StartCoroutine(Vanish());
             }
         }
     }
